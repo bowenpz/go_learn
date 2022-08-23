89e2e3b204ae67560078b5be74efe90e5a589238
zhangshuo.pz <zhangshuo.pz@bytedance.com>
2022-08-23 16:38:35 +0800

    feat(gorm): udpate gorm init

--- gorm_learn/config.go
+++ gorm_learn/config.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -13,16 +12,36 @@ var (
 	autoMigrateTables []interface{}
 )
 
-func init() {
-	var (
-		dbUser   = "root"
-		dbPasswd = "123456"
-		dbHost   = "127.0.0.1"
-		dbPort   = "3306"
-		dbName   = "mysql"
-		err      error
-	)
+var (
+	dbUser     = "root"
+	dbPasswd   = "123456"
+	dbHost     = "127.0.0.1"
+	dbPort     = "3306"
+	dpPort5738 = "33061"
+	dpPort800  = "33062"
+	dpPort8029 = "33063"
+	dbName     = "mysql"
+)
 
+func MySQLVersion800() {
+	if err := startGorm(dbUser, dbPasswd, dbHost, dpPort800, dbName); err != nil {
+		panic(err)
+	}
+}
+
+func MySQLVersion8029() {
+	if err := startGorm(dbUser, dbPasswd, dbHost, dpPort8029, dbName); err != nil {
+		panic(err)
+	}
+}
+
+func MySQLVersion5738() {
+	if err := startGorm(dbUser, dbPasswd, dbHost, dpPort5738, dbName); err != nil {
+		panic(err)
+	}
+}
+
+func startGorm(dbUser, dbPasswd, dbHost, dbPort, dbName string) error {
 	mysqlConfig := mysql.New(mysql.Config{
 		DSN: fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPasswd, dbHost, dbPort, dbName),
 	})
@@ -30,19 +49,23 @@ func init() {
 		DisableForeignKeyConstraintWhenMigrating: true,
 		Logger:                                   logger.Default.LogMode(logger.Info),
 	}
-	if db, err = gorm.Open(mysqlConfig, gormConfig); err != nil {
-		panic(err)
-	}
+	var err error
+	db, err = gorm.Open(mysqlConfig, gormConfig)
+	return err
 }
 
-func AutoMigrate() {
+func MigrateTable(tables ...interface{}) {
+	// table: optional pointer
+	if len(tables) > 0 {
+		return
+	}
+	autoMigrateTables = append(autoMigrateTables, tables...)
+	autoMigrate()
+}
+
+func autoMigrate() {
 	autoDB := db.Set("gorm:table_options", "ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 ROW_FORMAT = Dynamic")
 	if err := autoDB.AutoMigrate(autoMigrateTables...); err != nil {
 		panic(err)
 	}
-}
-
-func MigrateTable(table interface{}) {
-	// table: optional pointer
-	autoMigrateTables = append(autoMigrateTables, table)
 }
--- gorm_learn/main.go
+++ gorm_learn/main.go
@@ -1,17 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"gorm.io/gorm/clause"
-)
-
 func main() {
-	AutoMigrate()
-
-	var department Department
-	res := db.Preload(clause.Associations).Where("department_id = ?", 1).Find(&department)
-	if res.Error != nil {
-		panic(res.Error)
-	}
-	fmt.Printf("%v", department)
+	MySQLVersion5738()
+	MigrateTable()
 }
